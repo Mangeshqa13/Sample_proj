0bb8925550d272fa0cc5058a1349259b6e2b1753
Alok sahu <aloksahu92april@gmail.com>
2015-12-28 15:53:05 +0530

    committed

---
+++ src/test/java/com/qedge/Google_launch.java
@@ -0,0 +1,13 @@
+package com.qedge;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Google_launch {
+
+	public static void main(String[] args) {
+		FirefoxDriver driver=new FirefoxDriver();
+		driver.get("http://google.co.in");
+
+	}
+
+}
